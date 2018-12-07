ef194fc8a472c0eb31dd38c3585db5d23f6e8d97
Parzival6 <jhowell@umass.edu>
2018-12-07 03:22:28 -0500

    Developed initialization routines

--- client.c
+++ client.c
@@ -9,9 +9,8 @@ int main(int argc, char **argv)
     int clientfd;
     char *host, *port, buf[MAXLINE];
     rio_t rio;
-
-    if (argc != 3) {
-	fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
+    if (argc != 4) {
+	fprintf(stderr, "usage: %s <host> <port> <username>\n", argv[0]);
 	exit(0);
     }
     host = argv[1];
@@ -19,12 +18,23 @@ int main(int argc, char **argv)
 
     clientfd = Open_clientfd(host, port);
     Rio_readinitb(&rio, clientfd);
-
+    strcat(buf, argv[3]);
+    strcat(buf, "\n");
+    Rio_writen(clientfd, buf, strlen(buf)); // Send username immediately
+    printf("[debug] -username- sent (before newline): -%s-\n", argv[3]);
+    while(1); //TODO: Remove/replace with code
+    
+    //TODO: make another thread, have one do sending, one do receiving, or whatever the website said
+    
+    /*
+    //Example input sending/receiving echo loop
     while (Fgets(buf, MAXLINE, stdin) != NULL) {
 	Rio_writen(clientfd, buf, strlen(buf));
 	Rio_readlineb(&rio, buf, MAXLINE);
 	Fputs(buf, stdout);
     }
+    */
+    
     Close(clientfd); //line:netp:echoclient:close
     exit(0);
 }
--- server.c
+++ server.c
@@ -6,17 +6,65 @@
 
 void *thread(void *vargp);
 
-void echo(int connfd)
+struct clientInfo {
+    struct clientInfo* next;
+    char* username;
+    int connfd;
+};
+
+struct clientInfo* firstClient = NULL;
+
+struct clientInfo* addClient(int connfd) {
+    struct clientInfo* newClient = (struct clientInfo*) malloc(sizeof(struct clientInfo));
+    newClient->connfd = connfd;
+    
+    if(firstClient == NULL) {
+        //TODO: mutex start?
+        firstClient = newClient;
+        //TODO: mutex end?
+    } else {
+        struct clientInfo* lastClient = firstClient;
+        while(lastClient->next != NULL) {
+            lastClient = lastClient->next;
+        }
+        //TODO: mutex start?
+        lastClient->next = newClient;
+        //TODO: mutex end?
+    }
+    
+    return newClient;
+}
+
+void clientHandlerStart(struct clientInfo* info)
 {
+    int connfd = info->connfd;
     int n;
     char buf[MAXLINE];
     rio_t rio;
 
     Rio_readinitb(&rio, connfd);
+    n = Rio_readlineb(&rio, buf, MAXLINE); // Capture username that client immediately sends
+    
+    // Copy username from buffer, except for the trailing newline
+    char* usrSlot = malloc(sizeof(char) * n); // Only n (not n+1) because stripping newline
+    for(int i=0; i<n-1 && i<MAXLINE; i++)
+        usrSlot[i] = buf[i];
+    usrSlot[n-1] = '\0'; // Add string terminator
+    info->username = usrSlot;
+    
+    printf("[debug] -username- received: -%s-\n", info->username);
+    while(1); //TODO: Remove/replace with code
+    
+    //TODO: make loop to handle client (and any more initialization, if needed)
+    
+    
+    /*
+    //Example input receiving/send echo loop
     while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
         printf("server received %d bytes\n", n);
         Rio_writen(connfd, buf, n);
     }
+    */
 }
 
 int main(int argc, char **argv) 
@@ -34,20 +82,21 @@ int main(int argc, char **argv)
 
     while (1) {
         clientlen=sizeof(struct sockaddr_storage);
-	connfdp = Malloc(sizeof(int)); //line:conc:echoservert:beginmalloc
-	*connfdp = Accept(listenfd, (SA *) &clientaddr, &clientlen); //line:conc:echoservert:endmalloc
-	Pthread_create(&tid, NULL, thread, connfdp);
+        connfdp = Malloc(sizeof(int)); //line:conc:echoservert:beginmalloc
+        *connfdp = Accept(listenfd, (SA *) &clientaddr, &clientlen); //line:conc:echoservert:endmalloc
+        struct clientInfo* newClient = addClient(*connfdp);
+        Free(connfdp);                    //line:conc:echoservert:free
+        Pthread_create(&tid, NULL, thread, &newClient);
     }
 }
 
 /* Thread routine */
 void *thread(void *vargp) 
-{  
-    int connfd = *((int *)vargp);
+{
+    struct clientInfo* newClient = (struct clientInfo*) *((struct clientInfo**) vargp);
     Pthread_detach(pthread_self()); //line:conc:echoservert:detach
-    Free(vargp);                    //line:conc:echoservert:free
-    echo(connfd);
-    Close(connfd);
+    clientHandlerStart(newClient);
+    Close(newClient->connfd);
     return NULL;
 }
 /* $end echoservertmain */
