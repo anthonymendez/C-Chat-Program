76686146bce9680b18ed7057f778b516ba9e13c7
Anthony Mendez <anthonymendez9@gmail.com>
2018-12-07 20:21:11 -0500

    Client: Main creates sender and receiver threads, fixed thread function arguments

--- client.c
+++ client.c
@@ -12,12 +12,14 @@ char *host, *port, buf[MAXLINE];
 rio_t rio;
 int killProgram;
 
-void *thread(enum ThreadType threadType);
+void *thread(void *vargp);
 void senderRoutine();
 void receiverRoutine();
 void sendDataToServer();
 
 int main(int argc, char **argv) {
+    pthread_t tid;
+
     if (argc != 4) {
         fprintf(stderr, "usage: %s <host> <port> <username>\n", argv[0]);
 	    exit(0);
@@ -26,13 +28,22 @@ int main(int argc, char **argv) {
     port = argv[2];
 
     killProgram = 0;
+    enum ThreadType S = Sender,
+                    R = Receiver;
+
     clientfd = Open_clientfd(host, port);
     Rio_readinitb(&rio, clientfd);
+
     strcat(buf, argv[3]);
     strcat(buf, "\n");
     Rio_writen(clientfd, buf, strlen(buf)); // Send username immediately
+
     printf("[debug] -username- sent (before newline): -%s-\n", argv[3]);
-    while(1); //TODO: Remove/replace with code
+
+    Pthread_create(&tid, NULL, thread, &S);
+    Pthread_create(&tid, NULL, thread, &R);
+
+    while(!killProgram); //TODO: Remove/replace with code
     
     //TODO: make another thread, have one do sending, one do receiving, or whatever the website said
     
@@ -50,7 +61,8 @@ int main(int argc, char **argv) {
 }
 
 /* Thread routine */
-void *thread(enum ThreadType threadType) {
+void *thread(void *vargp) {
+    enum ThreadType threadType = *((enum ThreadType*) vargp);
     Pthread_detach(pthread_self()); //line:conc:echoservert:detach
 
     // Launch Appropriate ThreadType
@@ -68,12 +80,12 @@ void *thread(enum ThreadType threadType) {
 
 /* Routine to reciever input and send messages to the server */
 void senderRoutine() {
-
+    printf("SenderRoutineThread\n");
 }
 
 /* Routine to listen to message from the server */
 void receiverRoutine() {
-
+    printf("ReceivRoutineThread\n");
 }
 
 /* Function to quickly send data in buffer to server */
